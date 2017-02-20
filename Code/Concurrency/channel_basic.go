5ae86d280bc49ae358758e8b9508e774b0aa7307
Harold Ramos <chattingharold@gmail.com>
2017-02-20 01:23:10 -0500

    Updated

--- Code/Concurrency/channel_basic.go
+++ Code/Concurrency/channel_basic.go
@@ -8,15 +8,15 @@ func say(ch chan string) {
 }
 
 func main() {
-	ch := make(chan string, 1)     // Creating channel string  with buffer size 1
+	ch := make(chan string, 1)     // Creating a string channel with a buffer size of 1
 	ch <- "channel inside of main" // Sending a string to the channel
 	fmt.Println(<-ch)              // Printing the channel
 
 	go say(ch)        // Calling the func say(takes a channel) returns a string
 	fmt.Println(<-ch) // Printing the channel
-	
+
 	/*
-	channel inside of main
-	channel outside of main
+		channel inside of main
+		channel outside of main
 	*/
 }
