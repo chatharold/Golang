6384c86fcdda38aa61fcd6166f78a489569f8fe3
Harold Ramos <chattingharold@gmail.com>
2017-02-20 01:20:09 -0500

    Updated

---
+++ Code/Concurrency/channel_basic.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Creating a function that takes a channel and returns a string to the channel
+func say(ch chan string) {
+	ch <- "channel outside of main"
+}
+
+func main() {
+	ch := make(chan string, 1)     // Creating channel string  with buffer size 1
+	ch <- "channel inside of main" // Sending a string to the channel
+	fmt.Println(<-ch)              // Printing the channel
+
+	go say(ch)        // Calling the func say(takes a channel) returns a string
+	fmt.Println(<-ch) // Printing the channel
+	
+	/*
+	channel inside of main
+	channel outside of main
+	*/
+}
