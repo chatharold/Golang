f158a90a7340fa1ed793eb7121a22e45b8cdbd1d
Harold Ramos <chattingharold@gmail.com>
2017-03-19 22:46:07 -0400

    updated

--- Code/Templates/01/main.go
+++ Code/Templates/01/main.go
@@ -5,5 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println("templates are comming soom...")
+	// Passing name variable
+	name := "Harold Ramos"
+
+	// Template
+	tpl := `
+	<!doctype html>
+	<html>
+    	<head></head>
+	<body>
+	<h1> Welcome: ` + name + `</h1>
+	</body>
+	</html>
+	`
+
+	// Output
+	fmt.Println(tpl)
+
+	/*
+			Result:
+			<!doctype html>
+		        <html>
+		        <head></head>
+		        <body>
+		        <h1> Welcome: Harold Ramos</h1>
+		        </body>
+		        </html>
+	*/
 }
